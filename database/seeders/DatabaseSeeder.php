f80be1940d4e6554e0c8538e6ff4d1c00ded8e65
Glenn Steven Santoso <glennstevencg21@gmail.com>
2023-08-04 22:47:49 +0700

    edit database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
+use Illuminate\Support\Str;
+
 use App\Models\Roles;
 use App\Models\User;
 
@@ -15,12 +17,18 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        \App\Models\User::factory()->create([
+        // \App\Models\User::factory()->create([
+        //     'name' => 'Glenn Steven',
+        //     'email' => 'glenn@test.com',
+        //     'password' => bcrypt('password'),
+        // ]);
+        User::create([
             'name' => 'Glenn Steven',
             'email' => 'glenn@test.com',
             'password' => bcrypt('password'),
+            'remember_token' => str()->random(10),
+            'email_verified_at' => now(),
         ]);
-        
         // Roles::factory(3)->create();
         // User::factory(4)->create();
     }
