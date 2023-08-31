5c3d69e018901b7c441dac3e4ffa52b6d5d5ea85
Glenn Steven Santoso <glennstevencg21@gmail.com>
2023-08-31 23:02:35 +0700

    feat: add migration inside laporan, kategori, and user

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('no_telp')->unique();
             $table->string('password');
             $table->foreignId('role_id')->nullable();
             $table->rememberToken();
--- database/migrations/2023_08_31_141659_create_laporans_table.php
+++ database/migrations/2023_08_31_141659_create_laporans_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('laporans', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
