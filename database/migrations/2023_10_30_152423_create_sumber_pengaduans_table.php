5210941277ee8849468dbbc3f27128de19d9e316
Glenn Steven Santoso <glennstevencg21@gmail.com>
2023-11-02 22:50:32 +0700

    fix: fix sumber_pengaduans field

--- database/migrations/2023_10_30_152423_create_sumber_pengaduans_table.php
+++ database/migrations/2023_10_30_152423_create_sumber_pengaduans_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         //
         Schema::create('sumber_pengaduans', function (Blueprint $table) {
             $table->id();
+            $table->string('nama');
             $table->timestamps();
             $table->softDeletes();
         });
