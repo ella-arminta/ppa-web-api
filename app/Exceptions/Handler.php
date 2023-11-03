ad9d7794240d8eb3b7d4288846611b2adabb5023
Glenn Steven Santoso <glennstevencg21@gmail.com>
2023-11-03 15:37:05 +0700

    fix: fix image directory issue

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -32,80 +32,80 @@ class Handler extends ExceptionHandler
         });
     }
 
-    public function render($request, Throwable $e)
-    {
-        if ($e instanceof \Illuminate\Database\Eloquent\ModelNotFoundException) {
-            $model = class_basename($e->getModel());
+    // public function render($request, Throwable $e)
+    // {
+    //     if ($e instanceof \Illuminate\Database\Eloquent\ModelNotFoundException) {
+    //         $model = class_basename($e->getModel());
 
-            return $this->error(
-                "{$model} tidak ditemukan",
-                HttpResponseCode::HTTP_NOT_FOUND
-            );
-        } else {
-            switch (true) {
-                case $e instanceof \Illuminate\Validation\ValidationException:
-                    return $this->error(
-                        $e->validator->errors(),
-                        HttpResponseCode::HTTP_BAD_REQUEST
-                    );
-                case $e instanceof \Illuminate\Auth\Access\AuthorizationException:
-                    return $this->error(
-                        'Anda tidak memiliki hak akses',
-                        HttpResponseCode::HTTP_UNAUTHORIZED
-                    );
-                case $e instanceof \Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException:
-                    return $this->error(
-                        'Metode tidak diizinkan',
-                        HttpResponseCode::HTTP_METHOD_NOT_ALLOWED
-                    );
-                case $e instanceof \Symfony\Component\HttpKernel\Exception\BadRequestHttpException:
-                    return $this->error(
-                        'Bad request',
-                        HttpResponseCode::HTTP_BAD_REQUEST
-                    );
-                case $e instanceof \Symfony\Component\HttpKernel\Exception\TooManyRequestsHttpException:
-                    return $this->error(
-                        'Terlalu banyak requests',
-                        HttpResponseCode::HTTP_TOO_MANY_REQUESTS
-                    );
-                case $e instanceof \Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException:
-                    return $this->error(
-                        'Terlarang',
-                        HttpResponseCode::HTTP_FORBIDDEN
-                    );
-                case $e instanceof \Symfony\Component\HttpKernel\Exception\ConflictHttpException:
-                    return $this->error(
-                        'Data telah tersedia',
-                        HttpResponseCode::HTTP_CONFLICT
-                    );
-                case $e instanceof \Symfony\Component\HttpKernel\Exception\UnsupportedMediaTypeHttpException:
-                    return $this->error(
-                        'Jenis media tidak didukung',
-                        HttpResponseCode::HTTP_UNSUPPORTED_MEDIA_TYPE
-                    );
-                case $e instanceof \Symfony\Component\HttpKernel\Exception\UnprocessableEntityHttpException:
-                    return $this->error(
-                        'Entitas tidak dapat diproses',
-                        HttpResponseCode::HTTP_UNPROCESSABLE_ENTITY
-                    );
-                case $e instanceof \Symfony\Component\HttpKernel\Exception\ServiceUnavailableHttpException:
-                    return $this->error(
-                        'Servis tidak tersedia',
-                        HttpResponseCode::HTTP_SERVICE_UNAVAILABLE
-                    );
-                case $e instanceof \Symfony\Component\HttpKernel\Exception\NotAcceptableHttpException:
-                    return $this->error(
-                        'Tidak dapat diterima',
-                        HttpResponseCode::HTTP_NOT_ACCEPTABLE
-                    );
-                default:
-                    return $this->error(
-                        $e->getMessage(),
-                        HttpResponseCode::HTTP_INTERNAL_SERVER_ERROR
-                    );
-            }
-        }
+    //         return $this->error(
+    //             "{$model} tidak ditemukan",
+    //             HttpResponseCode::HTTP_NOT_FOUND
+    //         );
+    //     } else {
+    //         switch (true) {
+    //             case $e instanceof \Illuminate\Validation\ValidationException:
+    //                 return $this->error(
+    //                     $e->validator->errors(),
+    //                     HttpResponseCode::HTTP_BAD_REQUEST
+    //                 );
+    //             case $e instanceof \Illuminate\Auth\Access\AuthorizationException:
+    //                 return $this->error(
+    //                     'Anda tidak memiliki hak akses',
+    //                     HttpResponseCode::HTTP_UNAUTHORIZED
+    //                 );
+    //             case $e instanceof \Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException:
+    //                 return $this->error(
+    //                     'Metode tidak diizinkan',
+    //                     HttpResponseCode::HTTP_METHOD_NOT_ALLOWED
+    //                 );
+    //             case $e instanceof \Symfony\Component\HttpKernel\Exception\BadRequestHttpException:
+    //                 return $this->error(
+    //                     'Bad request',
+    //                     HttpResponseCode::HTTP_BAD_REQUEST
+    //                 );
+    //             case $e instanceof \Symfony\Component\HttpKernel\Exception\TooManyRequestsHttpException:
+    //                 return $this->error(
+    //                     'Terlalu banyak requests',
+    //                     HttpResponseCode::HTTP_TOO_MANY_REQUESTS
+    //                 );
+    //             case $e instanceof \Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException:
+    //                 return $this->error(
+    //                     'Terlarang',
+    //                     HttpResponseCode::HTTP_FORBIDDEN
+    //                 );
+    //             case $e instanceof \Symfony\Component\HttpKernel\Exception\ConflictHttpException:
+    //                 return $this->error(
+    //                     'Data telah tersedia',
+    //                     HttpResponseCode::HTTP_CONFLICT
+    //                 );
+    //             case $e instanceof \Symfony\Component\HttpKernel\Exception\UnsupportedMediaTypeHttpException:
+    //                 return $this->error(
+    //                     'Jenis media tidak didukung',
+    //                     HttpResponseCode::HTTP_UNSUPPORTED_MEDIA_TYPE
+    //                 );
+    //             case $e instanceof \Symfony\Component\HttpKernel\Exception\UnprocessableEntityHttpException:
+    //                 return $this->error(
+    //                     'Entitas tidak dapat diproses',
+    //                     HttpResponseCode::HTTP_UNPROCESSABLE_ENTITY
+    //                 );
+    //             case $e instanceof \Symfony\Component\HttpKernel\Exception\ServiceUnavailableHttpException:
+    //                 return $this->error(
+    //                     'Servis tidak tersedia',
+    //                     HttpResponseCode::HTTP_SERVICE_UNAVAILABLE
+    //                 );
+    //             case $e instanceof \Symfony\Component\HttpKernel\Exception\NotAcceptableHttpException:
+    //                 return $this->error(
+    //                     'Tidak dapat diterima',
+    //                     HttpResponseCode::HTTP_NOT_ACCEPTABLE
+    //                 );
+    //             default:
+    //                 return $this->error(
+    //                     $e->getMessage(),
+    //                     HttpResponseCode::HTTP_INTERNAL_SERVER_ERROR
+    //                 );
+    //         }
+    //     }
 
-        return parent::render($request, $e);
-    }
+    //     return parent::render($request, $e);
+    // }
 }
