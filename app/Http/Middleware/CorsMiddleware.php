f97228aea4b32a8ab0d36d8e6be2db52bcc3499b
sidipraptama <87693364+sidipraptama@users.noreply.github.com>
2023-11-13 14:11:35 +0700

    coba cors 5

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -3,6 +3,7 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use Illuminate\Support\Facades\Log;
 
 class CorsMiddleware
 {
@@ -13,6 +14,10 @@ class CorsMiddleware
             ->header('Access-Control-Allow-Origin', '*')
             ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS')
             ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+        // Debugging
+        $methodsHeader = $response->headers->get('Access-Control-Allow-Methods');
+        Log::info('Access-Control-Allow-Methods Header: ' . $methodsHeader);
 
         return $response;
     }
--- config/cors.php
+++ config/cors.php
@@ -17,8 +17,9 @@ return [
 
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
-    // 'allowed_methods' => ['*'],
-    'allowed_methods' =>  ['GET', 'POST', 'PUT', 'PATCH', 'DELETE','POST'],
+    'allowed_methods' => ['*'],
+    // 'allowed_methods' => ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+
 
     'allowed_origins' => ['*'],
 
