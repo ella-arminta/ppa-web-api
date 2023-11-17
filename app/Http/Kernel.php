3b0b52db781664b6cde19326f2851d7037dc52f3
Ella Arminta <85121822+ella-arminta@users.noreply.github.com>
2023-11-17 20:39:09 +0700

    fix: fix cors 6

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -21,7 +21,6 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
-        'cors' => \App\Http\Middleware\CorsMiddleware::class,
     ];
 
     /**
@@ -67,5 +66,6 @@ class Kernel extends HttpKernel
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'abilities' => \Laravel\Sanctum\Http\Middleware\CheckAbilities::class,
         'ability' => \Laravel\Sanctum\Http\Middleware\CheckForAnyAbility::class,
+        'cors' => \App\Http\Middleware\CorsMiddleware::class,
     ];
 }
