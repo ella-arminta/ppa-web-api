d3ecaa5c0a0e63cbf16f6308c4e641a1782608f2
Ella Arminta <85121822+ella-arminta@users.noreply.github.com>
2023-11-17 12:07:23 +0700

    feat: post keluarga berupa array

--- app/Http/Controllers/KeluargaKlienController.php
+++ app/Http/Controllers/KeluargaKlienController.php
@@ -29,4 +29,27 @@ class KeluargaKlienController extends BaseController
 
         return $this->success($data->resource, HttpResponseCode::HTTP_OK);
     }
+
+    public function store(Request $request)
+    {
+        // kalau kirim array of keluarga
+        if($request->has('data') && is_array($request->data)){
+            $hasil = [];
+            foreach($request->data as $key => $value){
+                $this->validateRequest($value, $this->model);
+                $data = $this->service->create($value);
+                $hasil[] = $data;
+            }
+            return $this->success($hasil, HttpResponseCode::HTTP_CREATED);
+        }else{ //kirim 1 keluarga
+
+            $this->validateRequest($request->all(), $this->model);
+            // store
+            $data = $this->service->create($request->all());
+            // return
+            return $this->success($data, HttpResponseCode::HTTP_CREATED);
+        }
+
+
+    }
 }
