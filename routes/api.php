bf0ed002aa581dfd54e53adc169aac081d145d2f
Ella Arminta <85121822+ella-arminta@users.noreply.github.com>
2023-11-18 19:28:52 +0700

    fix: cors

--- routes/api.php
+++ routes/api.php
@@ -49,8 +49,8 @@ Route::get('/testing', function () {
 
 // disini cek routing
 
-Route::middleware(['cors'])->group(function () {
-// Route::middleware(['App\Http\Middleware\CorsMiddleware'])->group(function () {
+// Route::middleware(['cors'])->group(function () {
+Route::middleware(['App\Http\Middleware\CorsMiddleware'])->group(function () {
     Route::controller(AuthController::class)->group(function () {
         Route::post('/login', 'login')->name('login');
         Route::post('/logout', 'logout')->name('logout')->middleware('auth:sanctum');
