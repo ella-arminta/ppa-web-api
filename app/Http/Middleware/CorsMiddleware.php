3ab3bea64febe14e13a8c3286b3705c7f28dcf49
Ella Arminta <85121822+ella-arminta@users.noreply.github.com>
2023-11-18 23:02:18 +0700

    fix: cors

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -13,8 +13,9 @@ class CorsMiddleware
         $response = $next($request)
             ->header('Access-Control-Allow-Origin', '*')
             ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS')
-            ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-
+            ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+            ->header('Access-Control-Allow-Credentials', 'true');
+         
         // Debugging
         // $methodsHeader = $response->headers->get('Access-Control-Allow-Methods');
         // Log::info('Access-Control-Allow-Methods Header: ' . $methodsHeader);
