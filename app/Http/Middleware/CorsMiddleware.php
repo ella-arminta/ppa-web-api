2ea6b61722527a0d57b9a1b2da2784cc5390882b
Ella Arminta <85121822+ella-arminta@users.noreply.github.com>
2023-11-18 22:33:27 +0700

    fix: cors

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -9,25 +9,25 @@ class CorsMiddleware
 {
     public function handle($request, Closure $next)
     {
-        // // Allow all origins
-        // $response = $next($request)
-        //     ->header('Access-Control-Allow-Origin', '*')
-        //     ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS')
-        //     ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+        // Allow all origins
+        $response = $next($request)
+            ->header('Access-Control-Allow-Origin', '*')
+            ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS')
+            ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
-        // // Debugging
-        // // $methodsHeader = $response->headers->get('Access-Control-Allow-Methods');
-        // // Log::info('Access-Control-Allow-Methods Header: ' . $methodsHeader);
-
-        // return $response;
-
-        $response = $next($request);
-
-        $response->headers->set('Access-Control-Allow-Origin', '*');
-        $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
-        $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+        // Debugging
+        // $methodsHeader = $response->headers->get('Access-Control-Allow-Methods');
+        // Log::info('Access-Control-Allow-Methods Header: ' . $methodsHeader);
 
         return $response;
+
+        // $response = $next($request);
+
+        // $response->headers->set('Access-Control-Allow-Origin', '*');
+        // $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
+        // $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+        // return $response;
     }
 }
 
