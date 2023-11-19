96a03a875e4c15fd22e057655a80b0e864d799c1
Ella Arminta <85121822+ella-arminta@users.noreply.github.com>
2023-11-19 21:08:55 +0700

    fix: cors again

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -9,26 +9,10 @@ class CorsMiddleware
 {
     public function handle($request, Closure $next)
     {
-        // Allow all origins
-        $response = $next($request)
-            ->header('Access-Control-Allow-Origin', '*')
-            ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS')
-            ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization')
-            ->header('Access-Control-Allow-Credentials', 'true');
-         
-        // Debugging
-        // $methodsHeader = $response->headers->get('Access-Control-Allow-Methods');
-        // Log::info('Access-Control-Allow-Methods Header: ' . $methodsHeader);
-
-        return $response;
-
-        // $response = $next($request);
-
-        // $response->headers->set('Access-Control-Allow-Origin', '*');
-        // $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
-        // $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-
-        // return $response;
+        return $next($request)
+        ->header('Access-Control-Allow-Origin', '*')
+        ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS')
+        ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     }
 }
 
