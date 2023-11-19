576f75a70a5e9f7c9b2be9353c038810f4d05a1c
Ella Arminta <85121822+ella-arminta@users.noreply.github.com>
2023-11-19 21:51:27 +0700

    debug: cors

--- app/Http/Middleware/CorsMiddleware2.php
+++ app/Http/Middleware/CorsMiddleware2.php
@@ -5,6 +5,7 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Facades\Log;
 
 class CorsMiddleware2
 {
@@ -15,8 +16,8 @@ class CorsMiddleware2
      */
     public function handle($request, Closure $next)
     {
-        \Log::info('CorsMiddleware is being executed');
-        
+        Log::info('CorsMiddleware is being executed');
+
         if ($request->isMethod('OPTIONS')) {
             return response('', 200)
                 ->header('Access-Control-Allow-Origin', '*')
