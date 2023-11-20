ccec3731eb591cd651de9ad99e4484d372162256
Glenn Steven Santoso <glennstevencg21@gmail.com>
2023-11-20 13:36:26 +0700

    fix: fix CORS issue #3

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -16,13 +16,14 @@ class Kernel extends HttpKernel
     protected $middleware = [
         // \App\Http\Middleware\TrustHosts::class,
         \App\Http\Middleware\TrustProxies::class,
-        // \Illuminate\Http\Middleware\HandleCors::class,
+        \Illuminate\Http\Middleware\HandleCors::class,
         \App\Http\Middleware\PreventRequestsDuringMaintenance::class,
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
+        \App\Http\Middleware\Cors::class,
     ];
-
+    
     /**
      * The application's route middleware groups.
      *
@@ -44,6 +45,7 @@ class Kernel extends HttpKernel
             // \Illuminate\Routing\Middleware\ThrottleRequests::class.':api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
             \App\Http\Middleware\CorsMiddleware2::class,
+            \App\Http\Middleware\Cors::class,
         ],
     ];
 
